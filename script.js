9fc6897a517e3edbd49f41c5c3ebc716e0dc9fb2
jordanoboyle <jmcmcode@gmail.com>
2024-09-11 23:35:39 -0600

    dialogue modal

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const modal = document.getElementById('modal');
 
 //Dialog Modal Practice
 const updateButton = document.getElementById('updateDetails');
-const cancel = document.getElementById('cancel');
-const dialog = document.getElementsByClassName('favDialog');
+const cancelButton = document.getElementById('cancel');
+const dialog = document.getElementById('favDialog');
 dialog.returnValue = 'favAnimal';
 
 //Toggle the Nav Bar:
@@ -35,3 +35,22 @@ window.addEventListener('click', (e) =>
 
 
 //Script Block for Favorite Animal Modal Dialog
+function openCheck(dialog) {
+  if (dialog.open) {
+    console.log('dialog box open');
+  } else {
+    console.log('dialog box closed');
+  }
+}
+// Update FAVAnimal button opens the modal dialog
+updateButton.addEventListener('click', () => {
+  dialog.showModal();
+  openCheck(dialog);
+});
+
+//Form cancel button closes the dialogue box
+cancelButton.addEventListener('click', () => {
+  dialog.close('animalNotChosen');
+  openCheck(dialog);
+})
+
